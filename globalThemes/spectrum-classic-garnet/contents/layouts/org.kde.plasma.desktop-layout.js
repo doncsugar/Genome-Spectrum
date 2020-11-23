d20b2868d6b8d691393d166c14ff3cb70e9c71ce
doncsugar <mandjandme@gmail.com>
2020-11-23 12:59:43 -0500

    testing a global theme

---
+++ globalThemes/spectrum-classic-garnet/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,177 @@
+var plasma = getApiVersion(1);
+
+var layout = {
+    "desktops": [
+        {
+            "applets": [
+                {
+                    "config": {
+                        "/": {
+                            "PreloadWeight": "0"
+                        }
+                    },
+                    "geometry.height": 0,
+                    "geometry.width": 0,
+                    "geometry.x": 0,
+                    "geometry.y": 0,
+                    "plugin": "org.kde.plasma.analogclock",
+                    "title": "Analog Clock"
+                }
+            ],
+            "config": {
+                "/": {
+                    "ItemGeometriesHorizontal": "Applet-46:1152,0,288,272,0;",
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "540",
+                    "DialogWidth": "720"
+                },
+                "/Configuration": {
+                    "PreloadWeight": "0"
+                },
+                "/General": {
+                    "ToolBoxButtonState": "topcenter",
+                    "ToolBoxButtonX": "667",
+                    "sortMode": "-1"
+                },
+                "/Wallpaper/org.kde.color/General": {
+                    "Color": "255,255,255"
+                },
+                "/Wallpaper/org.kde.image/General": {
+                    "Image": "file:///usr/share/wallpapers/Next/contents/images/1920x1080.jpg",
+                    "SlidePaths": "/home/don/.local/share/wallpapers,/usr/share/wallpapers"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "panels": [
+        {
+            "alignment": "left",
+            "applets": [
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "100"
+                        },
+                        "/Configuration/General": {
+                            "favoritesPortedToKAstats": "true"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.kickoff"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "0"
+                        },
+                        "/Configuration/ConfigDialog": {
+                            "DialogHeight": "540",
+                            "DialogWidth": "720"
+                        },
+                        "/Configuration/General": {
+                            "showOnlyCurrentScreen": "true",
+                            "showWindowIcons": "true",
+                            "wrapPage": "true"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.pager"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration/ConfigDialog": {
+                            "DialogHeight": "540",
+                            "DialogWidth": "720"
+                        },
+                        "/Configuration/General": {
+                            "groupingAppIdBlacklist": "org.kde.dolphin.desktop,org.kde.kate.desktop,org.kde.gwenview.desktop,firefox.desktop",
+                            "groupingLauncherUrlBlacklist": "applications:org.kde.dolphin.desktop,applications:org.kde.kate.desktop,applications:firefox.desktop,applications:org.kde.gwenview.desktop",
+                            "launchers": "applications:systemsettings.desktop,applications:org.kde.discover.desktop,preferred://filemanager,applications:org.inkscape.Inkscape.desktop",
+                            "middleClickAction": "ToggleGrouping"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.icontasks"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "100"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.systemtray"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "100"
+                        },
+                        "/Configuration/Appearance": {
+                            "enabledCalendarPlugins": "/usr/lib/x86_64-linux-gnu/qt5/plugins/plasmacalendarplugins/holidaysevents.so,/usr/lib/x86_64-linux-gnu/qt5/plugins/plasmacalendarplugins/astronomicalevents.so",
+                            "showDate": "true"
+                        },
+                        "/Configuration/ConfigDialog": {
+                            "DialogHeight": "540",
+                            "DialogWidth": "720"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.digitalclock"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "0"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.showdesktop"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "2",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "89",
+                    "DialogWidth": "1920"
+                },
+                "/Configuration": {
+                    "PreloadWeight": "0"
+                }
+            },
+            "height": 2.111111111111111,
+            "hiding": "normal",
+            "location": "bottom",
+            "maximumLength": 106.66666666666667,
+            "minimumLength": 106.66666666666667,
+            "offset": 0
+        }
+    ],
+    "serializationFormatVersion": "1"
+}
+;
+
+plasma.loadSerializedLayout(layout);
