aa7bb35b157dc1b5f3d4f602aa7cb55ae9a0bd1b
doncsugar <mandjandme@gmail.com>
2021-01-20 19:57:03 -0500

    added bad themes

--- plasmaThemes/classic/minimal/genome/genThemes.sh
+++ plasmaThemes/classic/minimal/genome/genThemes.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 #loop through themes
-for theme in spectrum-minimal-classic-basic spectrum-minimal-classic-light spectrum-minimal-classic-dark; do
+for theme in spectrum-minimal-classic-basic spectrum-minimal-classic-light spectrum-minimal-classic-dark spectrum-malice spectrum-spite; do
     echo "creating $theme"
     mkdir -p output/$theme
-    cp -a common/* output/$theme/
     cp -a $theme/* output/$theme/
+    cp -an common/* output/$theme/
 done
+
+echo cleaning bad themes
+
+rm -rf output/spectrum-spite/translucent
+rm -rf output/spectrum-malice/translucent
+
+echo done
