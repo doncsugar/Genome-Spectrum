ffd64393f59e0f9f2f0b655824e45ca9650d7f3e
doncsugar <mandjandme@gmail.com>
2021-03-01 22:35:31 -0500

    rolling out updates, fixed bad themes

--- plasmaThemes/classic/minimal/genome/genThemes.sh
+++ plasmaThemes/classic/minimal/genome/genThemes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #loop through themes
-for theme in spectrum-minimal-classic-basic spectrum-minimal-classic-light spectrum-minimal-classic-dark spectrum-malice spectrum-spite; do
+for theme in spectrum-minimal-classic-basic spectrum-minimal-classic-light spectrum-minimal-classic-dark spectrum-malice spectrum-malice-opaque spectrum-spite spectrum-spite-opaque; do
     echo "creating $theme"
     mkdir -p output/$theme
     cp -a $theme/* output/$theme/
@@ -12,5 +12,7 @@ echo cleaning bad themes
 
 rm -rf output/spectrum-spite/translucent
 rm -rf output/spectrum-malice/translucent
+rm -rf output/spectrum-spite-opaque/translucent
+rm -rf output/spectrum-malice-opaque/translucent
 
 echo done
