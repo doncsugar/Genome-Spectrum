89e5416dca86294c8f28df37b37d44bf7c5ef648
doncsugar <mandjandme@gmail.com>
2021-03-14 00:18:14 -0500

    testing opacity changing script

--- plasmaThemes/classic/minimal/genome/genThemes.sh
+++ plasmaThemes/classic/minimal/genome/genThemes.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 #loop through themes
-for theme in spectrum-minimal-classic-basic spectrum-minimal-classic-light spectrum-minimal-classic-dark spectrum-malice spectrum-malice-opaque spectrum-spite spectrum-spite-opaque; do
+for theme in spectrum-classic-basic spectrum-classic-basic-opaque spectrum-classic-light spectrum-classic-dark spectrum-malice spectrum-malice-opaque spectrum-spite spectrum-spite-opaque; do
     echo "creating $theme"
     mkdir -p output/$theme
     cp -a $theme/* output/$theme/
     cp -an common/* output/$theme/
 done
 
-echo cleaning bad themes
+echo cleaning opaque themes
 
 rm -rf output/spectrum-spite/translucent
 rm -rf output/spectrum-malice/translucent
 rm -rf output/spectrum-spite-opaque/translucent
 rm -rf output/spectrum-malice-opaque/translucent
+
+rm -rf output/spectrum-classic-basic-opaque/translucent
 
 echo done
