b2a4facf297c2f88a505eceec5df8b15ef31323d
doncsugar <mandjandme@gmail.com>
2021-04-20 23:31:59 -0400

    added opaque version of colors

--- plasmaThemes/classic/minimal/colors/genThemes.sh
+++ plasmaThemes/classic/minimal/colors/genThemes.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 #loop through themes
-for theme in spectrum-classic-colors spectrum-classic-charoite spectrum-classic-garnet spectrum-classic-iolite spectrum-classic-mawsitsit spectrum-classic-strawberryquartz; do
+for theme in spectrum-classic-colors spectrum-classic-colors-opaque spectrum-classic-charoite spectrum-classic-garnet spectrum-classic-iolite spectrum-classic-mawsitsit spectrum-classic-strawberryquartz; do
     echo "creating $theme"
     mkdir -p output/$theme
     cp -a common/* output/$theme/
     cp -a $theme/* output/$theme/
 done
+
+rm -rf output/spectrum-classic-colors-opaque/translucent
+rm -f output/spectrum-classic-colors-opaque/changeOpacity.sh
+
+echo done
